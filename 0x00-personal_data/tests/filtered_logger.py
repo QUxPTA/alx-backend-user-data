29ec6cc9b8b5b2fbf94d185f399b75bc61b861cc
kibuchijw <kibuchijw@gmail.com>
2024-06-02 22:30:54 +0300

    Update filtered_logger.py to have a get_logger function implementation

--- 0x00-personal_data/tests/filtered_logger.py
+++ 0x00-personal_data/tests/filtered_logger.py
@@ -4,26 +4,26 @@ Personal Data logging tasks
 """
 import logging
 import re
+from typing import List
+
+PII_FIELDS = ("name", "email", "phone", "SSN", "password")
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """
     Obfuscates specified fields in a log message.
 
     Args:
         fields (list): List of strings representing all fields to obfuscate.
-        redaction (str): String representing by what
-        the field will be obfuscated.
+        redaction (str): String representing by what the field will be obfuscated.
         message (str): String representing the log line.
-        separator (str): String representing by which character
-        is separating all fields in the log line.
+        separator (str): String representing by which character is separating all fields in the log line.
 
     Returns:
         str: The obfuscated log message.
     """
     pattern = f"({'|'.join(fields)})=[^{separator}]*"
-    return re.sub(pattern,
-                  lambda m: f"{m.group().split('=')[0]}={redaction}", message)
+    return re.sub(pattern, lambda m: f"{m.group().split('=')[0]}={redaction}", message)
 
 
 class RedactingFormatter(logging.Formatter):
@@ -34,13 +34,12 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
         """
         Initialize the formatter with specified fields to obfuscate.
 
         Args:
-            fields (list): List of strings representing all
-            fields to obfuscate.
+            fields (list): List of strings representing all fields to obfuscate.
         """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
@@ -56,8 +55,27 @@ class RedactingFormatter(logging.Formatter):
             str: The formatted log record with obfuscated fields.
         """
         original_message = super().format(record)
-        return filter_datum(self.fields,
-                            self.REDACTION, original_message, self.SEPARATOR)
+        return filter_datum(self.fields, self.REDACTION, original_message, self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """
+    Creates and configures a logger.
+
+    Returns:
+        logging.Logger: Configured logger object.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    return logger
 
 
 if __name__ == '__main__':
