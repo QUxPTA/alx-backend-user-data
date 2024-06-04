7b4fc98b8bb41e53cf47fa416e3775e55b62dfa7
kibuchijw <kibuchijw@gmail.com>
2024-06-04 17:05:07 +0300

    Update api/v1/auth/basic_auth.py by adding a method in the class BasicAuth that returns the Base64 part of the Authorization header for a Basic Authentication

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,21 @@ class BasicAuth(Auth):
     """
     BasicAuth class for basic authentication
     """
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        Extract the Base64 part of the Authorization header for Basic Authentication
+
+        Args:
+            authorization_header (str): The Authorization header
+
+        Returns:
+            str: The Base64 part of the Authorization header, or None if invalid
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header.split(" ", 1)[1]
