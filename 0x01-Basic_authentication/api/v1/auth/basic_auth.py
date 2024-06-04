771ddbaad9051338d269f950e3e55ad747449ec8
kibuchijw <kibuchijw@gmail.com>
2024-06-04 18:24:18 +0300

    Update api/v1/auth/basic_auth.py, add a method in the class BasicAuth that returns the user email and password from the Base64 decoded value

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -49,3 +49,23 @@ class BasicAuth(Auth):
             return decoded_bytes.decode('utf-8')
         except ValueError:
             return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Extract the user email and password from the decoded Base64 value
+
+        Args:
+            decoded_base64_authorization_header (str): The decoded Base64 value
+
+        Returns:
+            tuple: The user email and password, or None, None if invalid
+        """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+        user_email, user_password = decoded_base64_authorization_header.split(
+            ":", 1)
+        return user_email, user_password
