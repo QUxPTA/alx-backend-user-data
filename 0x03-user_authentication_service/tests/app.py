a1ba5506e7a3bad043969288de354aad9a6234b6
kibuchijw <kibuchijw@gmail.com>
2024-06-10 19:08:24 +0300

    Add  a basic Flask app

---
+++ 0x03-user_authentication_service/tests/app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Basic Flask app module"""
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def home():
+    """
+    GET /
+    Return a JSON payload with a welcome message.
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
