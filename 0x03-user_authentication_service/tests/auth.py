6aa1e2d2f28b6937dbeebb055cebf2e2418da700
kibuchijw <kibuchijw@gmail.com>
2024-06-11 10:02:07 +0300

    Update auth.py, implement the Auth.get_user_from_session_id method

--- 0x03-user_authentication_service/tests/auth.py
+++ 0x03-user_authentication_service/tests/auth.py
@@ -101,3 +101,22 @@ class Auth:
             return sess_id
         except NoResultFound:
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """
+        Retrieve the user's email associated with the given session ID.
+
+        Args:
+            session_id (str): The session ID to look up.
+
+        Returns:
+            str: The email of the user associated with the session ID,
+            or None if no user is found.
+        """
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user.email
+        except NoResultFound:
+            return None
