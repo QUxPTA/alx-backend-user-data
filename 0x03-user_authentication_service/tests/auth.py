22eb311bc3b6787ccb8811a12b26cf5c1c5ff8a6
kibuchijw <kibuchijw@gmail.com>
2024-06-11 11:19:33 +0300

    Update auth.py, implement the Auth.update_password method. It takes reset_token string argument and a password string argument and returns None

--- 0x03-user_authentication_service/tests/auth.py
+++ 0x03-user_authentication_service/tests/auth.py
@@ -155,3 +155,15 @@ class Auth:
             return reset_token
         except NoResultFound:
             raise ValueError("No user found with the provided email")
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        update_password.
+        """
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            self._db.update_user(user.id,
+                                 hashed_password=_hash_password(password),
+                                 reset_token=None)
+        except NoResultFound:
+            raise ValueError
